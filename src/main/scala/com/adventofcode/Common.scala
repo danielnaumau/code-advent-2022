1bab47fb4a1f2bca7e648f142c498d448306ba33
danielnaumau <sadfreddy@gmail.com>
2022-12-01 13:04:34 +0200

    add day1

--- src/main/scala/com/adventofcode/Common.scala
+++ src/main/scala/com/adventofcode/Common.scala
@@ -4,6 +4,28 @@ import scala.io.Source
 import scala.util.Using
 
 object Common {
-  def readFile[T](path: String, process: List[String] => T): Option[T] =
-    Using(Source.fromFile(path))(file => process(file.getLines().toList)).toOption
+  def readFile[T](path: String, convert: List[String] => T): Option[T] =
+    Using(Source.fromFile(path))(file => convert(file.getLines().toList)).toOption
+
+  final case class NonEmptyList[A](head: A, tail: List[A] = Nil) {
+    def map[V](fun: A => V): NonEmptyList[V] =
+      NonEmptyList(fun(head), tail.map(fun))
+
+    def last: A =
+      tail.lastOption.getOrElse(head)
+
+    def toList: List[A] = head +: tail
+
+    def contains(value: A): Boolean =
+      tail.contains(value) || value == head
+
+    def exists(fun: A => Boolean): Boolean =
+      tail.exists(fun) || fun(head)
+
+    def +:(value: A): NonEmptyList[A] =
+      this.copy(value, tail = head +: tail)
+
+    def :+(value: A): NonEmptyList[A] =
+      this.copy(tail = tail :+ value)
+  }
 }
--- src/main/scala/com/adventofcode/Day1.scala
+++ src/main/scala/com/adventofcode/Day1.scala
@@ -1,5 +1,29 @@
 package com.adventofcode
 
-object Day1 {
+import com.adventofcode.Common.NonEmptyList
 
+
+object Day1 {
+  def task1(calories: List[Int]): Int =
+    calories.max
+
+  def task2(calories: List[Int]): Int =
+    calories.sorted.reverse.take(3).sum
+
+  def convert(lines: List[String]): List[Int] = {
+    lines.foldRight(NonEmptyList(0)) { case (line, values) =>
+      if (line.isEmpty)
+        0 +: values
+      else {
+        NonEmptyList(values.head + line.toIntOption.getOrElse(0), values.tail)
+      }
+    }.toList
+  }
+
+  def main(args: Array[String]): Unit = {
+    val calories = Common.readFile("src/main/resources/day1/task1.txt", convert)
+
+    println(calories.map(task1))
+    println(calories.map(task2))
+  }
 }
